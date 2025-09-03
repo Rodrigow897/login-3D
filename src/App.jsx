00a3f225d3661d734c5fa06f845e0a61c71b13ed
Rodrigo <rwesley2000@gmail.com>
2025-09-02 21:18:24 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="neumorphic neumorphic-card">
+        <h1>Login</h1>
+        <input type="text" className='neumorphic neumorphic-input'
+        placeholder='Username' />
+        <input type="password" className='neumorphic 
+        neumorphic-input' placeholder='Password' />
+        <button className='neumorphic neumorphic-button'>Sing In</button>
+      </div>
+    </>
+  )
+}
+
+export default App
